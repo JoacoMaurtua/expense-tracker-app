bb20637ba77c8b4692ff03e374119cc8413db489
JoacoMaurtua <joacomaurtua@gmail.com>
2022-10-12 00:37:36 -0500

    primera config navegacion

--- App.js
+++ App.js
@@ -5,25 +5,83 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import RecentExpScreen from './screens/RecentExpScreen';
 import AllExpScreen from './screens/AllExpScreen';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+
 
 const Stack = createNativeStackNavigator();
 
 const Tab = createBottomTabNavigator();
+
+function MyTabsNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#20b2aa' },
+        headerTintColor: 'white',
+        tabBarActiveBackgroundColor: '#20b2aa',
+        tabBarInactiveBackgroundColor: '#20b2aa',
+        tabBarActiveTintColor: '#ffd700',
+        tabBarInactiveTintColor: '#f8f8ff',
+      }}
+    >
+      <Tab.Screen
+        name="Recent Expenses"
+        component={RecentExpScreen}
+        options={{
+          title: 'Recent Expenses',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="timer-sand-empty"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="All Expenses"
+        component={AllExpScreen}
+        options={{
+          title: 'All Expenses',
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign
+              name="calendar"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <>
       <StatusBar animated={true} backgroundColor="#20b2aa" style="light" />
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Recent expenses" component={RecentExpScreen} />
-          <Tab.Screen name="All expenses" component={AllExpScreen} />
-        </Tab.Navigator>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="MyTabs"
+            component={MyTabsNavigator}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
       </NavigationContainer>
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  
-});
+/* const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 60,
+  },
+}); */
--- screens/RecentExpScreen.js
+++ screens/RecentExpScreen.js
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function RecentExpScreen() {
+export default function RecentExpScreen({navigation}) {
   return (
     <View>
       <Text>RecentExpScreen</Text>
