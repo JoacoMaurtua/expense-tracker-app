52e32a1263504991b544dd014b8114f4b2a8474e
JoacoMaurtua <joacomaurtua@gmail.com>
2022-10-12 00:46:03 -0500

    icono plus

---
+++ components/IconButton.js
@@ -0,0 +1,23 @@
+import { Pressable, StyleSheet } from "react-native";
+import {Ionicons} from '@expo/vector-icons';
+
+const IconButton = ({onPress, icon, color}) => {
+  return (
+    <Pressable onPress={onPress} style={({pressed}) => pressed && styles.pressed}>
+      <Ionicons name={icon} size={24} color={color} style={styles.icon}/>
+    </Pressable>
+  )
+}
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  pressed:{
+    opacity: 0.5,
+  },
+
+  icon:{
+    fontSize: 30,
+    marginRight: 20,
+  }
+})
--- screens/RecentExpScreen.js
+++ screens/RecentExpScreen.js
@@ -1,7 +1,28 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
+import IconButton from '../components/IconButton';
 
 export default function RecentExpScreen({navigation}) {
+
+  function navigateHandler(){
+    console.log('pressed!')
+  }
+
+  useLayoutEffect(()=>{
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            icon='md-add'
+            onPress={navigateHandler}
+            color="white"
+          />
+        );
+      },
+    });
+
+  },[navigation,navigateHandler]);
+
   return (
     <View>
       <Text>RecentExpScreen</Text>
