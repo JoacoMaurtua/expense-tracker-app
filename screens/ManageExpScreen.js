ae347cc639ae63f29f7185c07ae32e7b08a99d8d
JoacoMaurtua <joacomaurtua@gmail.com>
2022-10-18 13:59:46 -0500

    delete button

--- screens/ManageExpScreen.js
+++ screens/ManageExpScreen.js
@@ -1,26 +1,55 @@
-import { View, Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import React, { useLayoutEffect } from 'react';
+import IconButton from '../components/IconButton';
+import { GlobalStyles } from '../styles';
 
 export default function ManageExpScreen({ navigation, route }) {
-
   const editedExpenseId = route.params?.expenseId;
 
-  const isEditing = !!editedExpenseId;  //con vierto en booleano editedExpenseId acorde a si se cumple o no su condicion
+  const isEditing = !!editedExpenseId; //con vierto en booleano editedExpenseId acorde a si se cumple o no su condicion
 
   //establecer titulo acorde a si se edita o crea un expense
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: isEditing? 'Edit Expense': 'Add Expense'
-    })
-  },[navigation, isEditing])
+      title: isEditing ? 'Edit Expense' : 'Add Expense',
+    });
+  }, [navigation, isEditing]);
 
+  function deleteExpense() {
+    console.log('delete expense');
+  }
 
   return (
-    <View>
-      <Text>ManageExpScreen</Text>
+    <View style={styles.container}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpense}
+          />
+        </View>
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800
+  },
+
+  deleteContainer:{
+    marginTop: 16,
+    padding: 8,
+    borderTopWidth: 2, 
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center'
+  },
+})
 
 /* 
 con route.params?.expenseId --> Condicion: Si params no es undefined
