04957168055a005efffcb027c175441f9ca04b06
JoacoMaurtua <joacomaurtua@gmail.com>
2022-10-19 16:54:25 -0500

    estructura useReducer

--- context/manageExpensesContext.js
+++ context/manageExpensesContext.js
@@ -1,22 +1,52 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from 'react';
 
 export const ManageExpensesContext = createContext({
-  ids:[],
-  addExpense: ()=>{},
-  removeExpense: (id)=>{},
-  editExpense: (id)=>{},
+  expenses: [],
+  addExpense: ({ title, amount, date }) => {},
+  removeExpense: (id) => {},
+  editExpense: (id, { title, amount, date }) => {},
 });
 
+//reducer
+function expensesReducer(state, action) {
+  switch (action.type) {
+    case 'ADD':
+    case 'UPDATE':
+    case 'DELETE':
+    default:
+      return state;
+  }
+}
 
-function ExpensesContextProvider({children}){
-  return(
-    <ManageExpensesContext.Provider>
+const value = {};
+
+function ExpensesContextProvider({ children }) {
+  const [expensesState, dispatch] = useReducer(expensesReducer);
+
+  //actions:
+
+  function addExpense(expenseData) {
+    dispatch({ type: 'ADD', payload: expenseData });
+  };
+
+  function deleteExpense(id) {
+    dispatch({ type: 'DELETE', payload: id });
+  };
+
+  function updateExpense(id, expenseData) {
+    dispatch({type: 'UPDATE', payload: {id,expenseData}})
+  }
+
+  return (
+    <ManageExpensesContext.Provider value={value}>
       {children}
     </ManageExpensesContext.Provider>
-  )
-
-
-};
-
+  );
+}
 
 export default ExpensesContextProvider;
+
+//Esquema: useReducer(<reductor>, <estado inicial>)
+
+//Nota: De alguna manera useReducer tiene la misma mecanica que useState
+//      pero con varias piezas de estado a la vez
