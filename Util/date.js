960e5fbde4ab3611ce85e07ab9916a0826d0e031
JoacoMaurtua <joacomaurtua@gmail.com>
2022-10-20 14:02:30 -0500

    funcion getDateMinusDays

--- Util/date.js
+++ Util/date.js
@@ -1,3 +1,13 @@
+//Obtener el formato de la fecha
 export function getFormattedDate(date){
   return `${date.getFullYear()}/${date.getMonth(1)}/${date.getDate()}`
+};
+
+//Obtener la fecha de una cantidad especifica de dias
+export function getDateMinusDays(date,days){
+  return newDate(date.getFullYear(), date.getMonth(), date.getDate() - days)
 }
+
+//new Date() --> 10 de Octubre
+
+//getDateMinusDays(new Date(), 7) --> 3 de Octubre
