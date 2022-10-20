fb8d2d4f964b8bf6cdf5591a19418652929e1f6c
JoacoMaurtua <joacomaurtua@gmail.com>
2022-10-20 13:43:28 -0500

    reducer de expenses terminado

--- context/manageExpensesContext.js
+++ context/manageExpensesContext.js
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from 'react';
+import { tempExpenses } from '../Util/DUMMY';
 
 export const ManageExpensesContext = createContext({
   expenses: [],
@@ -7,38 +8,50 @@ export const ManageExpensesContext = createContext({
   editExpense: (id, { title, amount, date }) => {},
 });
 
-//reducer
-function expensesReducer(state, action) {
+//reducer(logica de cada funcion)
+function expensesReducer(state, action) { //state es un arreglo de expenses(objetos)
   switch (action.type) {
     case 'ADD':
+      const id = new Date().toDateString + Math.random().toString();
+      return [{ ...action.payload, id: id }, ...state]; //retorno (objeto con expenseData + id provicional) + expenses ya existentes
+    
     case 'UPDATE':
+      const updatableExpenseIndex = state.findIndex( //determinar la posicion del objeto a actualizar
+        (expense) => expense.id === action.payload.id
+      );
+      const updatableExpense = state[updatableExpenseIndex] //el objeto a actualziar
+      const updatedItem = {...updatableExpense, ...action.payload.data} //objeto con los nuevos datos sobreescritos
+      const updatedExpenses = [...state] //arreglo inicial
+      updatedExpenses[updatableExpenseIndex] = updatedItem //reemplazo el nuevo objeto por el anterior en el arreglo
+      return updatedExpenses;
+
     case 'DELETE':
+      return state.filter((expense) => expense.id !== action.payload)
+
     default:
       return state;
   }
 }
 
-const value = {};
 
 function ExpensesContextProvider({ children }) {
-  const [expensesState, dispatch] = useReducer(expensesReducer);
+  const [expensesState, dispatch] = useReducer(expensesReducer, tempExpenses);
 
   //actions:
-
   function addExpense(expenseData) {
     dispatch({ type: 'ADD', payload: expenseData });
-  };
+  }
 
   function deleteExpense(id) {
     dispatch({ type: 'DELETE', payload: id });
-  };
+  }
 
   function updateExpense(id, expenseData) {
-    dispatch({type: 'UPDATE', payload: {id,expenseData}})
+    dispatch({ type: 'UPDATE', payload: { id:id, data:expenseData } });
   }
 
   return (
-    <ManageExpensesContext.Provider value={value}>
+    <ManageExpensesContext.Provider value={expensesState}>
       {children}
     </ManageExpensesContext.Provider>
   );
