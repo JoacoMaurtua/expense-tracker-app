da16ecc8913a9a97bce4747777ef5becc50b94b6
JoacoMaurtua <joacomaurtua@gmail.com>
2022-10-24 17:29:02 -0500

    estilos input

--- components/Input.js
+++ components/Input.js
@@ -1,11 +1,34 @@
-import { View, TextInput, Text} from 'react-native'
-import React from 'react'
+import { View, TextInput, Text, StyleSheet} from 'react-native'
+import React from 'react';
+import { GlobalStyles } from '../styles';
 
 export default function Input({label,textInputConfig}) {
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig}/>
+    <View style={styles.inputContainer}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput style={styles.input} {...textInputConfig}/>
     </View>
   )
-}
+};
+
+const styles = StyleSheet.create({
+  inputContainer:{
+    marginHorizontal: 4, 
+    marginVertical: 16
+  },
+
+  label:{
+    fontSize: 12, 
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4
+  },
+
+  input:{
+    backgroundColor: GlobalStyles.colors.primary100,
+    color: GlobalStyles.colors.primary700,
+    padding: 6,
+    borderRadius: 6, 
+    fontSize: 18,
+  }
+})
+
