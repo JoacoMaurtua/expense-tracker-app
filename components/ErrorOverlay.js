8f4b63718f74a9c2884e0e94b8d3303c6c83f66a
JoacoMaurtua <joacomaurtua@gmail.com>
2022-11-03 00:19:26 -0500

    estructura error overlay

---
+++ components/ErrorOverlay.js
@@ -0,0 +1,34 @@
+import { View, Text, StyleSheet, Button } from 'react-native'
+import React from 'react'
+import { GlobalStyles } from '../styles'
+
+export default function ErrorOverlay({message, onConfirm}) {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.text, styles.title]}>A error ocurred!</Text>
+      <Text style={styles.text}>{message}</Text>
+      <Button onPress={onConfirm}>Acept</Button>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    padding:25,
+    textAlign: 'center',
+    alignItems: 'center',
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+
+  text:{
+    color: 'white',
+    textAlign: 'center',
+    marginBottom: 8,
+  },
+
+  title:{
+    fontSize:20,
+    fontWeight: 'bold',
+  }
+})
